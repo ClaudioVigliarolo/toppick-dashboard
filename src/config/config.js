bd1d61b8f2195d7bc59e7a5ff0fa6847952fd7f5
Claudio Vigliarolo <claudio.vigliarolo@kerr-italy.it>
2021-05-10 11:58:26 +0200

    temp

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOSTNAME:
     process.env.NODE_ENV === "production"
-      ? "https://top-pick-api.herokuapp.com"
+      ? "https://top-pick-api-dev.herokuapp.com"
       : "http://localhost:4001",
 };
