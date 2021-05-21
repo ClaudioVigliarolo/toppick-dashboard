76d088e24b461270d79e3cfe15ebbd392e7acd21
Claudio Vigliarolo <claudio.vigliarolo@kerr-italy.it>
2021-05-21 20:20:26 +0200

    added loading dialog and fixed bugs

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  HOSTNAME:
-    process.env.NODE_ENV === "production"
-      ? "https://top-pick-api.herokuapp.com"
-      : "http://localhost:4001",
-};
-//http://localhost:4001 https://top-pick-api-dev.herokuapp.com
