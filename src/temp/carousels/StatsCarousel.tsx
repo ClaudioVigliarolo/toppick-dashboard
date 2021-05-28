ad388a47157cd683171762bd9cbf627cb78a673b
Claudio Vigliarolo <claudio.vigliarolo@kerr-italy.it>
2021-05-28 23:38:15 +0200

    added source select

--- src/temp/carousels/StatsCarousel.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Carousel from "react-material-ui-carousel";
-
-interface StatsCarouselProps {
-  items: React.ReactNode[];
-}
-export default function StatsCarousel({ items }: StatsCarouselProps) {
-  return <Carousel>{items}</Carousel>;
-}
